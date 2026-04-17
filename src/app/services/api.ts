206496683f50ae580a63a8d9004a7e4581875f56
muhabbat98 <bmpmuhabbatpulatova98@gmail.com>
2026-04-17 17:21:01 +0500

    api changes

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL?import.meta.env.VITE_API_URL+'api' : 'http://localhost:3001/api';
+const BASE_URL = import.meta.env.VITE_API_URL?import.meta.env.VITE_API_URL+'/api' : 'http://localhost:3001/api';
 
 async function request<T>(
   method: string,
